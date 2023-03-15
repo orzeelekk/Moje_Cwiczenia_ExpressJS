25e673af741fae2da2846803432f1b18f204192f
orzeelekk <orzellpawell@gmail.com>
2023-03-15 11:36:20 +0100

    wyk zad 03

--- 01_Wprowadzenie_do_ExpressJS/03_Debugowanie_z_modulem_debug/app.js
+++ 01_Wprowadzenie_do_ExpressJS/03_Debugowanie_z_modulem_debug/app.js
@@ -9,12 +9,15 @@ const prodMyApp = debug('prod');
 app.get('/',(req,res) => {
     debugMyApp(`Method: ${req.method}`)
     debugMyApp(`Headers: ${req.headers}`)
+    debugMyApp('important data here')
     prodMyApp('Endpoint uruchomiony')
-    res.send('Hello World!')
+    //po DEBUG=* node app.js mamy dodatkowo konsole z dev/prodami dev important data here i prod endpoint..
+    res.send('Hello World !')
+    //wyswietlane tylko res.send w postman
 })
 app.listen(3000, () => {
     debugMyApp('Listening on 3000')
     console.log('Listening on 3000')
 })
 //DEBUG=* node app.js
-//DEBUG=dev,prod node app.js
+//DEBUG=dev,prod node app.js -> tu mi juz nic nie wyswielta dodatkowego
