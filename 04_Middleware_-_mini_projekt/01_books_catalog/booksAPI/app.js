0f2f39760f812dae0cf2072d8adb1e7e59e20ddf
orzeelekk <orzellpawell@gmail.com>
2023-03-21 19:18:56 +0100

    wyk zad 07

--- 04_Middleware_-_mini_projekt/01_books_catalog/booksAPI/app.js
+++ 04_Middleware_-_mini_projekt/01_books_catalog/booksAPI/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import {router as books} from "./routers/books.js";
 import morgan from 'morgan';
+import compression from 'compression';
 
 const app = express();
 const port = 3000;
 
 app.use('/books',books)
 app.use(morgan('combined'));
+app.use(compression())
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
