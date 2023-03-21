71c16955c241dc73afa9834105e9559d602e9700
orzeelekk <orzellpawell@gmail.com>
2023-03-21 17:44:47 +0100

    wyk zad 05

--- 04_Middleware_-_mini_projekt/01_books_catalog/booksAPI/routers/books.js
+++ 04_Middleware_-_mini_projekt/01_books_catalog/booksAPI/routers/books.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import {getBooks,saveBooks} from "../utils/utils.js";
+import bodyParser from "body-parser";
 
 export const router = express.Router();
 
@@ -16,4 +17,78 @@ router.get('/',async(req,res,next) => {
 router.use((err,res,next) => {
     const error = { status:res.statusCode, error: err.message };
     res.send(error)
+})
+
+//tworzymy aplikacje json parser
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extented: false })
+
+router.param('id',async (req,res,next,id) => {
+    try {
+        const books = await getBooks();
+
+        const book = books.find((book) => book.id === id);
+        const index = books.indexOf(book)
+
+        if (book && ['PUT','DELETE'].includes(req.method)) {
+            req.index = index
+            req.book = book
+            next()
+        } else if (!book && ['PUT','DELETE'].includes(req.method)) {
+            res.status(409)
+            throw new Error('Book doesnt exist!')
+        } else if (book && 'POST' === req.method) {
+            res.status(409)
+            throw new Error('Book already exist!')
+        } else {
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.put('/:id',urlencodedParser,async (req,res,next) => {
+    try {
+        const books = await getBooks()
+        const book = {...req.book, ...req.body}
+        const newBooks = [
+            ...books.slice(0,req.index),
+            book,
+            ...books.slice(req.index + 1),
+        ];
+        saveBooks(newBooks)
+        res.json(newBooks)
+    } catch (err) {
+        res.status(409)
+        next(err)
+    }
+});
+
+router.post('/:id',jsonParser,async (req,res,next) => {
+    try {
+        const books = await getBooks()
+        const newBooks = [...books]
+        newBooks.push(req.body)
+        saveBooks(newBooks)
+        res.json(newBooks)
+    } catch (err) {
+        res.status(409)
+        next(err)
+    }
+})
+
+router.delete('/id:',async (req,res,next) => {
+    try {
+        const books = await getBooks()
+        const newBooks = [
+            ...books.slice(0,req.index),
+            ...books.slice(req.index + 1),
+        ];
+        saveBooks(newBooks)
+        res.json(newBooks)
+    } catch (err) {
+        res.status(409)
+        next(err)
+    }
 })
