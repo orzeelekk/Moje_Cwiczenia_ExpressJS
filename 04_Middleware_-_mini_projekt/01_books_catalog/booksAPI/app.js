e1cbc001823d605fe69425c22434bf99a75e73f8
orzeelekk <orzellpawell@gmail.com>
2023-03-21 19:16:24 +0100

    zad 05 uzupelnienie i 6

--- 04_Middleware_-_mini_projekt/01_books_catalog/booksAPI/app.js
+++ 04_Middleware_-_mini_projekt/01_books_catalog/booksAPI/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import {router as books} from "./routers/books.js";
+import morgan from 'morgan';
 
 const app = express();
 const port = 3000;
 
 app.use('/books',books)
+app.use(morgan('combined'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
