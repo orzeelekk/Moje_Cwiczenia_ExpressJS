5f44a414b58cc7cd6a0ea706dd8d8312bc036ed0
orzeelekk <orzellpawell@gmail.com>
2023-03-22 11:28:49 +0100

    wyk zad 01

--- 02_Req-res_w_ExpressJS/01_http_to_express/app.js
+++ 02_Req-res_w_ExpressJS/01_http_to_express/app.js
@@ -1,48 +1,63 @@
-import http from 'http';
-import url from 'url';
-import fs from 'fs';
-import path from 'path';
-import mime from 'mime';
+// import http from 'http';
+// import url from 'url';
+// import fs from 'fs';
+// import path from 'path';
+// import mime from 'mime';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+import express from 'express';
 
-const findAsset = (name) => {
-  return new Promise((resolve, reject) => {
-    const assetPath = path.resolve(`./public/${name}`);
-    fs.readFile(assetPath, { encoding: 'utf-8' }, (err, asset) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(asset);
-      }
-    });
-  });
-};
-
-const router = {
-  '/ GET': {
-    asset: 'index.html',
-    type: mime.getType('html'),
-  },
-  '/style.css GET': {
-    asset: 'style.css',
-    type: mime.getType('css'),
-  },
-};
-
-const server = http.createServer(async (req, res) => {
-  const method = req.method;
-  const route = url.parse(req.url).pathname;
-  const routeMatch = router[`${route} ${method}`];
-  const { type, asset } = routeMatch;
-
-  res.writeHead(200, { 'Content-Type': type });
-
-  res.write(await findAsset(asset));
+const app = express();
+app.use(express.static('public'));
+app.get('/',(req,res) => {
   res.end();
-});
+})
+//to tez dziala
+// app.post('/',(req,res) => {
+//   console.log(req.body)
+//   res.json(req.body)
+// })
+app.listen(3000, () => {console.log('Listening on 3000')})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+
+// const findAsset = (name) => {
+//   return new Promise((resolve, reject) => {
+//     const assetPath = path.resolve(`./public/${name}`);
+//     fs.readFile(assetPath, { encoding: 'utf-8' }, (err, asset) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(asset);
+//       }
+//     });
+//   });
+// };
+//
+// const router = {
+//   '/ GET': {
+//     asset: 'index.html',
+//     type: mime.getType('html'),
+//   },
+//   '/style.css GET': {
+//     asset: 'style.css',
+//     type: mime.getType('css'),
+//   },
+// };
+//
+// const server = http.createServer(async (req, res) => {
+//   const method = req.method;
+//   const route = url.parse(req.url).pathname;
+//   const routeMatch = router[`${route} ${method}`];
+//   const { type, asset } = routeMatch;
+//
+//   res.writeHead(200, { 'Content-Type': type });
+//
+//   res.write(await findAsset(asset));
+//   res.end();
+// });
+//
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
