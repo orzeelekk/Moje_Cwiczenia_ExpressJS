4dc1e0d7e6c821e1ea31d8e1ce2d6d5c0b02e671
orzeelekk <orzellpawell@gmail.com>
2023-03-22 13:14:11 +0100

    wyk zad 06

--- 02_Req-res_w_ExpressJS/02_Routing_i_middleware_wstep/app.js
+++ 02_Req-res_w_ExpressJS/02_Routing_i_middleware_wstep/app.js
@@ -65,5 +65,31 @@ app.post('/books/:isbn', async function(req, res, next) {
         next(err);
     }
 });
+app.put('/books/:isbn',async function(req,res,next) {
+    try {
+        const books = await getBooks()
+        const bookIndex = books.findIndex((book) => book.isbn === req.params.isbn);
+
+        if (booksIndex === -1) {
+            const err = new Errror('Books is already on the list Mr')
+            res.status(409)
+            next(err)
+        } else {
+            const updatedBook = { ...books[bookIndex], ...req.body};
+            const updatedBooks = [
+                ...books.slice(0,bookIndex),
+                updatedBook,
+                ...books.slice(bookIndex,booksIndex + 1),
+            ];
+            await fs.promises.writeFile(
+                './data/books.json',
+                JSON.stringify(updatedBooks)
+            );
+            res.json(await getBooks());
+        }
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 app.listen(3000, () => {console.log('Listening on 3000')})
